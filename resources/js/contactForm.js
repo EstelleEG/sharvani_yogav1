862211e24444e6c4b293348842c294e49e6eaf41
Estelle <estellegregoire0689@gmail.com>
2022-06-10 11:30:48 +0200

    updates

--- resources/js/contactForm.js
+++ resources/js/contactForm.js
@@ -3,6 +3,7 @@
     emailjs.init("4riFHalEW_nXH8pM7");
   })();
   
+  //get the inputs
   function sendmail() {
     let fullName = document.getElementById("name").value;
     let userEmail = document.getElementById("email").value;
@@ -16,3 +17,32 @@
   
     emailjs.send('service_vbfmzcj', 'template_zm0k9yn', contactParams).then(function (res) { })
   }
+
+  //check and send confirmation
+  function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
+//or 
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
